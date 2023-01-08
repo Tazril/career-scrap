2ad2465104817f03650d3b6b5820d302db0ed3fc
tazrilparveezali <tazril.ali@cred.club>
2023-01-08 23:51:47 +0530

    bootstrap

--- main.py
+++ main.py
@@ -6,14 +6,36 @@ from driver import MainDriver
 
 driver = MainDriver()
 app = Flask(__name__)
+LIMIT = 100
+
+
+def get_jobs(request):
+    text = None
+    if request.method == 'POST':
+        text = request.form['search']
+    if request.args.get('search'):
+        text = request.args.get('search')
+    if text:
+        jobs = driver.repository.search_postings(text)
+    else:
+        jobs = driver.repository.get_postings()
+    return text, jobs
+
+
+def get_paginated_jobs(jobs):
+    page = 0
+    if request.args.get('page'):
+        page = int(request.args.get('page'))
+    total = (len(jobs) + LIMIT - 1) // LIMIT
+    paginated = jobs[page * LIMIT: (page + 1) * LIMIT]
+    return paginated, page, total
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        text = request.form['search']
-        return render_template('index.html', jobs=driver.repository.search_postings(text), search=text)
-    return render_template('index.html', jobs=driver.repository.get_postings())
+    text, jobs = get_jobs(request)
+    paginated, page, total = get_paginated_jobs(jobs)
+    return render_template('index.html', jobs=paginated, search=text, total=total, page=page)
 
 
 @app.route("/refresh")
