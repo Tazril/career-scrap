b00b1c93cbc784f35f42c6ad5a31745f44ccc2c3
tazrilparveezali <tazril.ali@cred.club>
2023-01-08 14:44:29 +0530

    thread

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ from flask import Flask, render_template, request
 from driver import MainDriver
 
 driver = MainDriver()
-
-
-class FlaskApp(Flask):
-    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-        driver.start()
-        super(FlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
-
-
-app = FlaskApp(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    if not driver.is_alive():
+        driver.start()
     if request.method == 'POST':
         text = request.form['search']
         return render_template('index.html', jobs=driver.repository.search_postings(text), search=text)
