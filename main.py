52bd186ff3cc67c151001b0bbd0fa33713961856
tazrilparveezali <tazril.ali@cred.club>
2023-01-08 17:04:33 +0530

    refresh

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template, request
 
 from driver import MainDriver
@@ -8,12 +10,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if not driver.is_alive():
-        driver.start()
     if request.method == 'POST':
         text = request.form['search']
         return render_template('index.html', jobs=driver.repository.search_postings(text), search=text)
     return render_template('index.html', jobs=driver.repository.get_postings())
+
+
+@app.route("/refresh")
+def refresh():
+    driver.update_data()
+    return "Data Refreshed " + str(datetime.datetime.now())
 
 
 if __name__ == '__main__':
