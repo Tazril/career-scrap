33001128b8221e1ed6872271d1932435e195c461
tazrilparveezali <tazril.ali@cred.club>
2023-01-12 22:07:37 +0530

    coursera

--- companies/coursera.py
+++ companies/coursera.py
@@ -7,21 +7,20 @@ from base.bsjobscrapper import BSJobScrapper
 class CourseraBSJobScrapper(BSJobScrapper):
 
     def get_content(self, url):
-        return  requests.get(url).content
-
+        return requests.get(url).content
 
     def get_name(self):
         return "Coursera"
 
     def get_image_url(self):
-        return "https://images.ctfassets.net/00atxywtfxvd/2MlqAOzmHjSPtssv6HlNox/1cb35b40775835a5f574ebc5509907a1/coursera-wordmark-blue.svg"
+        return "https://w7.pngwing.com/pngs/898/376/png-transparent-united-states-coursera-massive-open-online-course-education-united-states-blue-text-trademark.png"
 
     def get_jobs(self):
         url = 'https://boards.greenhouse.io/embed/job_board?for=coursera'
         content = self.get_content(url)
         soup = BeautifulSoup(content, 'html.parser')
         postings = []
-        for child in soup.find_all('div',  class_="opening"):
+        for child in soup.find_all('div', class_="opening"):
             postings.append({})
             postings[-1]['title'] = child.find('a').get_text().strip()
             postings[-1]['location'] = child.find('span').get_text().strip()
